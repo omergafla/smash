eb46ae102163b5b510ff24788b543fcde0f27066
omergafla <omergafla@gmail.com>
2020-11-19 08:56:39 +0000

    ctrl-Z now supported! using WUNTRACED flag for waitpid

--- signals.cpp
+++ signals.cpp
@@ -2,18 +2,30 @@
 #include <signal.h>
 #include "signals.h"
 #include "Commands.h"
+#include <unistd.h> 
 
 using namespace std;
 
 void ctrlZHandler(int sig_num) {
 	// TODO: Add your implementation
   cout << "smash: got ctrl-Z" <<endl;
-
+  pid_t pid = SmallShell::getInstance().current_pid;
+  if(pid != getpid()){
+    kill(pid, SIGSTOP);
+    cout << "process " << pid << " was stopped" <<endl;
+  }
 }
 
 void ctrlCHandler(int sig_num) {
   // TODO: Add your implementation
-  cout << "smash: got ctrl-C omer was here" <<endl;
+  pid_t pid = SmallShell::getInstance().current_pid;
+  cout << "smash: got ctrl-C" <<endl;
+  if(pid != getpid()){
+    kill(pid, SIGKILL);
+    cout << "process " << pid << " was killed" <<endl;
+  }
+
+  
 }
 
 void alarmHandler(int sig_num) {
